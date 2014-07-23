927ce51bffa02d354d4005e9babee70944fb11b7
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2014-07-23 23:56:47 +0100

    Check a blog post

--- src/test/scala/user-files/simulations/Foo.scala
+++ src/test/scala/user-files/simulations/Foo.scala
@@ -3,12 +3,22 @@ package simulations
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import assertions._
+import scala.concurrent.duration._
 
 class MySimulation extends Simulation {
-  val scn = scenario("Visit my blog")
-    .exec(http("My blog")
-      .get("https://tom-fitzhenry.me.uk/blog/"))
+  val myHttpProto = http.baseURL("https://tom-fitzhenry.me.uk/")
 
-  setUp(scn.inject(atOnce(10 users)))
-    .assertions(global.responseTime.percentile1.lessThan(750))
+  val myScn = scenario("Visit my blog")
+    .exec(http("Homepage")
+      .get("/blog/"))
+    .pause(1,4)
+    .exec(
+      http("Blog post")
+        .get("/blog/2012/11/personal-two-factor-authentication.html"))
+
+  setUp(myScn.inject(ramp(20 users) over (10 seconds)))
+    .protocols(myHttpProto)
+    .assertions(
+      global.successfulRequests.percent.is(100),
+      global.responseTime.percentile1.lessThan(750))
 }
